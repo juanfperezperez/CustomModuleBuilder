5664e16c37eb44eb94fe4e70e89a8e7e5ccad23e
Juan <lapcapfrank@yahoo.com>
2016-04-24 16:54:02 -0400

    Removed the reference to the Requred modules loader and added a check for Pester and PSScriptAnalyzer

--- CustomModuleBuilder.psm1
+++ CustomModuleBuilder.psm1
@@ -1,11 +1,15 @@
 $ModuleFile = $MyInvocation.MyCommand.Path
 $ModuleRoot = Split-Path -Path $ModuleFile -Parent
 
-Remove-Module -Name RequiredModulesLoader
-
 Write-Verbose "Importing Functions"
 #Import everything in the "Functions" folder
 "$ModuleRoot\Functions\*.ps1" |
     Resolve-Path |
         Where-Object {-not ($_.ProviderPath.Contains(".Tests."))} |
             ForEach-Object {. $_.ProviderPath; Write-Verbose $_.ProviderPath}
+
+$availableModules = Get-Module -ListAvailable -Name Pester,PSScriptAnalyzer | Select-Object -ExpandProperty Name
+
+if(('Pester' -notin $availableModules) -or ('PSScriptAnalyzer' -notin $availableModules)){
+    Write-Debug  -Message "Pester and PSScriptAnalyzer are required to run the tests included in this module"
+}
